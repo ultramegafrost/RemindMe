423fb65464c20dff34399f8847a962ea37194727
ultramegafrost <npfrost1>
2016-02-05 04:52:59 +0300

    Added Toolbar and Search button

---
+++ RemindMe/app/src/androidTest/java/com/example/remindme/ApplicationTestTest.java
@@ -0,0 +1,10 @@
+package com.example.remindme;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by frost on 05.02.2016.
+ */
+public class ApplicationTestTest extends TestCase {
+
+}
--- RemindMe/app/src/main/java/com/example/remindme/MainActivity.java
+++ RemindMe/app/src/main/java/com/example/remindme/MainActivity.java
@@ -2,14 +2,29 @@ package com.example.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-/**
- * Created by frost on 05.02.2016.
- */
 public class MainActivity extends Activity {
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
